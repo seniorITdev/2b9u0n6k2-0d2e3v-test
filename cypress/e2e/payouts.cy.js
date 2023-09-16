698ee1caa8f206d39c7c7a4521f2e1912f086263
stormchaser33 <nicolasthiry860@gmail.com>
2023-09-16 07:50:06 +0200

    complete test

--- cypress/e2e/payouts.cy.js
+++ cypress/e2e/payouts.cy.js
@@ -1,28 +1,38 @@
-describe('Payouts', () => {
-  it('Calculates correct payouts', () => {
-    cy.visit('http://localhost:4200');
+describe("Payouts", () => {
+  it("Calculates correct payouts", () => {
+    cy.visit("http://localhost:4200");
 
-    cy.get('[data-testid="name-input"]').type('Adriana');
-    cy.get('[data-testid="amount-input"]').type('5.75');
+    cy.get('[data-testid="name-input"]').type("Adriana");
+    cy.get('[data-testid="amount-input"]').type("5.75");
     cy.get('[data-testid="add-expense-button"]').click();
-    cy.get('[data-testid="name-input"]').type('Adriana');
-    cy.get('[data-testid="amount-input"]').type('5.75');
+    cy.get('[data-testid="name-input"]').type("Adriana");
+    cy.get('[data-testid="amount-input"]').type("5.75");
     cy.get('[data-testid="add-expense-button"]').click();
-    cy.get('[data-testid="name-input"]').type('Bao');
-    cy.get('[data-testid="amount-input"]').type('12');
+    cy.get('[data-testid="name-input"]').type("Bao");
+    cy.get('[data-testid="amount-input"]').type("12");
     cy.get('[data-testid="add-expense-button"]').click();
 
     cy.get('[data-testid="settle-up-button"]').click();
 
-    cy.get('[data-testid="total"]').should('contain.text', '23.5');
-    cy.get('[data-testid="equal-share"]').should('contain.text', '11.75');
-    cy.get('[data-testid="payouts-owes-0"]').should('contain.text', 'Adriana');
-    cy.get('[data-testid="payouts-owed-0"]').should('contain.text', 'Bao');
-    cy.get('[data-testid="payouts-amount-0"]').should('contain.text', '0.25');
+    cy.get('[data-testid="total"]').should("contain.text", "23.5");
+    cy.get('[data-testid="equal-share"]').should("contain.text", "11.75");
+    cy.get('[data-testid="payouts-owes-0"]').should("contain.text", "Adriana");
+    cy.get('[data-testid="payouts-owed-0"]').should("contain.text", "Bao");
+    cy.get('[data-testid="payouts-amount-0"]').should("contain.text", "0.25");
+  });
+
+  it("Check result", () => {
+    cy.visit("http://localhost:4200");
+
+    cy.get('[data-testid="name-input"]').type("asdfasdf");
+    cy.get('[data-testid="amount-input"]').type("50");
+    cy.get('[data-testid="add-expense-button"]').click();
+    cy.get('[data-testid="trash-icon-0"]').click();
+    cy.get('[data-testid="trash-icon-0"]').should("have.class", "fa-marked");
+    cy.get('[data-testid="edit-icon-0"]').click();
+    cy.get('[data-testid="edit-icon-0"]').should("have.class", "fa-marked");
+    cy.get('[data-testid="check-icon-0"]').click();
+    cy.get('[data-testid="check-icon-0"]').should("have.class", "fa-marked");
+    // cy.get("")
   });
 });
-
-
-
-
-
